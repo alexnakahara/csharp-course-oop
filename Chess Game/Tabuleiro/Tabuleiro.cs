c177c1313218bb3ac1335d6dcbb28d45b8592ad7
Alexander Nakahara <lexnakahara@gmail.com>
2023-01-18 08:58:35 -0300

    Handle error w/ Exception personalized

--- Chess Game/Tabuleiro/Tabuleiro.cs
+++ Chess Game/Tabuleiro/Tabuleiro.cs
@@ -13,15 +13,48 @@
             Pecas = new Peca[linhas, colunas];
         }
 
+        public Peca Peca(Posicao posicao)
+        {
+            return Pecas[posicao.Linha, posicao.Coluna];
+        }
+
         public Peca Peca(int linha, int coluna)
         {
             return Pecas[linha, coluna];
         }
 
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao pos)
         {
-            Pecas[pos.Linha, pos.Coluna] = p;   
-            p.Posicao= pos;
+            if (ExistePeca(pos))
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+
+            Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
         }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
+
     }
 }
---
+++ Chess Game/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg): base(msg) { }
+    }
+}
