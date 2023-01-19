b144c698d2b8c0cd16fc6e6753cb8275d6676d54
Alexander Nakahara <lexnakahara@gmail.com>
2023-01-19 13:34:52 -0300

    Add another pieces

---
+++ Chess Game/xadrez/Bispo.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Tasks.Dataflow;
+using tabuleiro;
+
+namespace xadrez
+{
+    public class Bispo : Peca
+    {
+        public Bispo(Tabuleiro tab, Cor cor): base(tab, cor) { }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool PodeMover(Posicao pos)
+        {
+            var p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            var pos = new Posicao(0, 0);
+
+            //Noroeste
+            pos.DefinirValor(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos) )
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor) 
+                {
+                    break;
+                }
+                pos.DefinirValor(pos.Linha - 1, pos.Coluna - 1);
+            }
+
+            //Nordeste
+            pos.DefinirValor(Posicao.Linha - 1, Posicao.Coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+
+               pos.DefinirValor(pos.Linha - 1, pos.Coluna + 1);
+            }
+
+            // Sudeste
+            pos.DefinirValor(Posicao.Linha + 1, Posicao.Coluna + 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.DefinirValor(pos.Linha + 1, pos.Coluna + 1);
+            }
+
+            // Sudoeste
+            pos.DefinirValor(Posicao.Linha + 1, Posicao.Coluna - 1);
+            while (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+
+                pos.DefinirValor(pos.Linha + 1, pos.Coluna - 1);
+            }
+
+            return mat;
+        }
+    }
+}
---
+++ Chess Game/xadrez/Cavalo.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading.Tasks.Dataflow;
+using tabuleiro;
+
+namespace xadrez
+{
+    public class Cavalo : Peca
+    {
+        public Cavalo(Tabuleiro tab, Cor cor): base(tab, cor) { }
+
+        public override string ToString()
+        {
+            return "C";
+        }
+
+        private bool PodeMover(Posicao pos)
+        {
+            var p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            var pos = new Posicao(0, 0);
+
+            pos.DefinirValor(Posicao.Linha - 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos) )
+            {
+               mat[pos.Linha, pos.Coluna] = true;
+            }
+            
+            pos.DefinirValor(Posicao.Linha - 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValor(Posicao.Linha - 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValor(Posicao.Linha - 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValor(Posicao.Linha + 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValor(Posicao.Linha + 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValor(Posicao.Linha + 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValor(Posicao.Linha + 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            return mat;
+        }
+    }
+}
