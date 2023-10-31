a43bf95da9e3fe80b5d17c2cd3459836998238f2
Alexander Nakahara <lexnakahara@gmail.com>
2023-10-31 01:29:56 -0300

    Directory and DirectoryInfo: Manipulation of  Directories

---
+++ FolderFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿
+#region Directory and DirectoryInfo
+
+var folderName = "pasta";
+var subFolderName = "subpasta";
+var subFolderNameStatic = "subpasta_usingstatic";
+
+var directoryInfoSubFolder = new DirectoryInfo(subFolderName);
+if (!Directory.Exists(subFolderName) || !directoryInfoSubFolder.Exists)
+{
+    Directory.CreateDirectory(subFolderNameStatic);
+    Directory.CreateDirectory(folderName);
+    directoryInfoSubFolder.Create();
+
+    directoryInfoSubFolder.MoveTo($"{folderName}//{subFolderName}");
+    Directory.Move(subFolderNameStatic, @$"{folderName}\{subFolderNameStatic}");
+}
+
+var name = directoryInfoSubFolder.Name;
+var parent = directoryInfoSubFolder.Parent;
+var root = directoryInfoSubFolder.Root;
+var exists = directoryInfoSubFolder.Exists;
+
+foreach (var directory in Directory.GetDirectories(folderName))
+{
+    Console.WriteLine(directory);
+}
+
+Directory.Delete($@"{folderName}/{subFolderName}");
+
+#endregion
+
+Console.ReadKey();
